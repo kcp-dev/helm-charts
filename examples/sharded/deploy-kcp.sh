03e0c7d633c6d29401bcfbd4592d2f4cc30b391f
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-01-05 13:14:44 +0200

    Update kcp operator to 0.4.0
    Signed-off-by: Mangirdas Judeikis <mangirdas@judeikis.lt>
    On-behalf-of: @SAP mangirdas.judeikis@sap.com

--- examples/sharded/deploy-kcp.sh
+++ examples/sharded/deploy-kcp.sh
@@ -2,20 +2,20 @@
 
 echo "Deploy KCP certificates…"
 
-helm upgrade --install --values kind-values-phase1.yaml --namespace kcp-certs --create-namespace kcp-certs ../../charts/certificates
-helm upgrade --install --values kind-values-phase2-alpha.yaml --namespace kcp-certs --create-namespace kcp-alpha ../../charts/certificates
-helm upgrade --install --values kind-values-phase2-beta.yaml --namespace kcp-certs --create-namespace kcp-beta ../../charts/certificates
-helm upgrade --install --values kind-values-phase2-proxy.yaml --namespace kcp-certs --create-namespace kcp-proxy ../../charts/certificates
-helm upgrade --install --values kind-values-phase2-cache.yaml --namespace kcp-certs --create-namespace kcp-cache ../../charts/certificates
+helm upgrade --install --values examples/sharded/kind-values-phase1.yaml --namespace kcp-certs --create-namespace kcp-certs charts/certificates
+helm upgrade --install --values examples/sharded/kind-values-phase2-alpha.yaml --namespace kcp-certs --create-namespace kcp-alpha charts/certificates
+helm upgrade --install --values examples/sharded/kind-values-phase2-beta.yaml --namespace kcp-certs --create-namespace kcp-beta charts/certificates
+helm upgrade --install --values examples/sharded/kind-values-phase2-proxy.yaml --namespace kcp-certs --create-namespace kcp-proxy charts/certificates
+helm upgrade --install --values examples/sharded/kind-values-phase2-cache.yaml --namespace kcp-certs --create-namespace kcp-cache charts/certificates
 
 echo "Deploy Cache server"
-helm upgrade --install --values kind-values-phase3-cache.yaml --namespace kcp-cache --create-namespace kcp-cache ../../charts/cache
+helm upgrade --install --values examples/sharded/kind-values-phase3-cache.yaml --namespace kcp-cache --create-namespace kcp-cache charts/cache
 
 echo "Deploy alpha root shard"
-helm upgrade --install --values kind-values-phase3-alpha.yaml --namespace kcp-alpha --create-namespace kcp-alpha ../../charts/shard
+helm upgrade --install --values examples/sharded/kind-values-phase3-alpha.yaml --namespace kcp-alpha --create-namespace kcp-alpha charts/shard
 
 echo "Deploy frontend proxy"
-helm upgrade --install --values kind-values-phase3-proxy.yaml --namespace kcp-proxy --create-namespace kcp-proxy ../../charts/proxy
+helm upgrade --install --values examples/sharded/kind-values-phase3-proxy.yaml --namespace kcp-proxy --create-namespace kcp-proxy charts/proxy
 
 echo "Deploy beta shard"
-helm upgrade --install --values kind-values-phase3-beta.yaml --namespace kcp-beta --create-namespace kcp-beta ../../charts/shard
+helm upgrade --install --values examples/sharded/kind-values-phase3-beta.yaml --namespace kcp-beta --create-namespace kcp-beta charts/shard
